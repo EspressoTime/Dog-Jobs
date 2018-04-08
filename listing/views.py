fa78ecb63ad92fce469451c4a37997405253a077
AVasquez <angela.k.vasquez@gmail.com>
2018-04-08 15:22:16 -0400

    Add newline.

--- listing/views.py
+++ listing/views.py
@@ -6,4 +6,5 @@ def post_list(request):
     listings = Listing.objects.all()
     for listing in listings:
     	listing.locs = listing.locations.all()
+    	
     return render(request, 'listing/post_list.html', {'listings': listings})
